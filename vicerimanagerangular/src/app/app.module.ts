fb4e26a610de95febba7fea23dab378b88b5c5a6
Rafael Coutinho <rafacout@gmail.com>
2018-12-09 10:46:20 -0200

    Added the home and header components

--- vicerimanagerangular/src/app/app.module.ts
+++ vicerimanagerangular/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ProjectComponent } from './projects/project/project.component';
 
 import { ProjectsService } from './projects/projects.service';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectsComponent,
-    ProjectComponent
+    ProjectComponent,
+    AboutComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ vicerimanagerangular/src/app/app.route.ts
@@ -0,0 +1,6 @@
+// import { Routes } from "@angular/router";
+
+
+// export const ROUTES: Routes = [
+//     {path: '', component: HomeComponent}
+// ];
